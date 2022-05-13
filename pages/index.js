056f4a888c1e2708923f1151c92864db9ce5fabe
Luis Rodriguez <luis@deepspacerobots.com>
2022-05-13 14:37:34 -0500

    indent fix

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ import Layout from "../components/layouts/layout";
 function HomePage() {
   return (
     <>
-    <Head>
-      <title>BizBio &bull; 1 link for all</title>
-    </Head>
+      <Head>
+        <title>BizBio &bull; 1 link for all</title>
+      </Head>
       <Layout>
         <div>Welcome to Next.js!</div>
       </Layout>
