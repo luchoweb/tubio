8f8d43c6afebd147e7f91294976c34ae05f78cca
Luis Rodriguez <luis@deepspacerobots.com>
2022-05-13 10:31:12 -0500

    some improvements and font icons added

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import Head from 'next/head';
+import Script from 'next/script';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.scss';
 import '../styles/components/layouts/layout.scss';
 import '../styles/components/layouts/business.scss';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <Script src="https://kit.fontawesome.com/4e1f6b21a2.js" strategy="afterInteractive"></Script>
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <meta charSet='utf-8' />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;600&display=swap" rel="stylesheet" />
         </Head>
         <body>
