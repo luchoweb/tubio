51158f1ec03a0e9a60e945da1b16bd2d26be9184
Luis Rodriguez <luis@deepspacerobots.com>
2022-05-14 17:25:12 -0500

    terms done

--- pages/terms.js
+++ pages/terms.js
@@ -6,13 +6,31 @@ function TermsPage() {
   return (
     <>
       <Head>
-        <title>Terminos y condiciones &bull; BizBio</title>
+        <title>Términos y condiciones &bull; BizBio</title>
       </Head>
       <Layout>
         <div className="container">
           <section className="pt-5 pb-5">
-            <h1>Términos y condiciones</h1>
-            <p>Pronto</p>
+            <h1 className="mb-5">Términos y condiciones</h1>
+            <p>TuBio es una plataforma gratuita en línea que permite a cualquier persona (en adelante Usuario) crear un perfil para uso personal o comercial. Teniendo lo anterior en cuenta, TuBio en la buena fe, entiende que cada Usuario es dueño o tiene los permisos suficientes para el uso de la información con la cual cree su perfil en esta plataforma (logo, nombre y enlaces).</p>
+            <p>TuBio habilita el correo electrónico tubio.link@gmail.com para recibir los reclamos provenientes por creación de perfiles usando información sin los permisos correspondientes, nos tomamos en serio el Copyright y la buena imagen de las personas y empresas.</p>
+            <p>TuBio pensando en la seguridad de usuarios, marcas y personas, verificará el mayor número de perfiles populares posibles, con ello se evita que personas inescrupulosas engañen por medio de perfiles falsos.</p>
+            <p>
+              <span>TuBio no usará, comercializará o difundirá la siguente información agregada por el usuario al momento de crear su perfil en la plataforma:</span>
+              <ul className="mt-3 mb-4">
+                <li>Logotipos</li>
+                <li>Imágenes</li>
+                <li>Nombres (usuario o marca)</li>
+                <li>Direcciones</li>
+                <li>Números telefónicos</li>
+              </ul>
+            </p>
+            <p>TuBio no usa cookies por el momento, ni para publicidad ni para mejorar la experiencia de uso.</p>
+            <p>TuBio podrá modificar estos términos y condiciones sin previo aviso.</p>
+            <p>TuBio usa licencias libres de programas (software) de código abierto para el funcionamiento de la aplicación y licencias gratuitas de otras plataformas como Google&reg; Fonts y Font Awesome de Fonticons, Inc.</p>
+            <p className="text-muted m-0 mt-5">
+              <small>Última revisión: 14 de mayo de 2022</small>
+            </p>
           </section>
         </div>
       </Layout>
