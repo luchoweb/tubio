7c8df61125c24c7bd492eda960296ce02b360e7a
Luis Rodriguez <luis@deepspacerobots.com>
2022-05-14 19:25:34 -0500

    new user and login form done

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/global.scss';
 import '../styles/components/layouts/layout.scss';
 import '../styles/components/layouts/business.scss';
+import '../styles/components/layouts/private.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return (
