66c106809edaf21ded9e26cf2fd030fe89203295
Luis Rodriguez <luis@deepspacerobots.com>
2022-05-20 19:47:46 -0500

    rollback command run

--- components/forms/business.js
+++ components/forms/business.js
@@ -2,8 +2,6 @@ import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 
-const { exec } = require('child_process');
-
 import ProfilePreview from '../profilePreview';
 import appScreen from "../../images/phone-screen-samsung.png";
 
@@ -27,8 +25,6 @@ function FormBiz() {
 
     if ( response.status === 200 ) {
       // Restart tubio next app
-      if ( process.env.NODE_ENV !== 'development' )
-        exec('sudo pm2 restart tubio', { encoding: 'utf-8' });
     } else {
       setError("Ha ocurrido un error creando su perfil, por favor haga clic nuevamente en Crear perfil.");
     }
