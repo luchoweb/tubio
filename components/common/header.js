3b0b095eda53c57c12ccac23ab8fe2fff190bfc5
Luis Rodriguez <luis@deepspacerobots.com>
2022-06-10 11:12:38 -0500

    header width updated

--- components/common/header.js
+++ components/common/header.js
@@ -5,13 +5,13 @@ export default function PublicHeader() {
   return (
     <header className="pt-3 pb-3">
       <div className="container">
-        <div className="row align-items-center">
-          <div className="col-6">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-6 col-lg-5">
             <Link href="/">
               <a><img src={Logo.src} alt={process.env.NEXT_PUBLIC_APP_NAME} height={59} width={117} /></a>
             </Link>
           </div>
-          <div className="col-6 d-flex justify-content-end">
+          <div className="col-6 col-lg-5 d-flex justify-content-end">
             <nav className="navigation">
               <ul className="list-unstyled m-0 p-0 d-flex align-items-center">
                 <li className="me-4">
