601bc59a6088333d095e46d91d21a695e7dfbd0f
Luis Rodriguez <luis@deepspacerobots.com>
2022-07-13 12:25:58 -0500

    media domain add to next config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const path = require('path')
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
+  },
+  images: {
+    domains: ['media.tubio.co'],
+  },
 }
