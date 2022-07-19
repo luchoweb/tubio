08ba4572879c116e669e5b68b700ac2581181791
Luis Rodriguez <luis@deepspacerobots.com>
2022-07-19 15:16:10 -0500

    removed temporally codify link on footer

--- components/common/footer.js
+++ components/common/footer.js
@@ -3,7 +3,7 @@ export default function PublicFooter() {
     <footer className="pt-3 pb-3">
       <p className="m-0 text-center">
         <small>
-          &copy; {process.env.NEXT_PUBLIC_APP_NAME} una marca <a href="https://codify.com.co" target="_blank" rel="noopener" className="text-muted"><strong>Codify</strong></a>
+          &copy; {process.env.NEXT_PUBLIC_APP_NAME} una marca <a href="#" rel="noopener" className="text-muted"><strong>Codify</strong></a>
         </small>
       </p>
     </footer>
